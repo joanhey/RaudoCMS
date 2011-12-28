9138f3899e1325fcde1f0292aa07b5161bf46641
joanhey <joanhey@kumbiaphp.com>
2011-12-28 21:57:04 +0100

    Empezando a crear estructura para el admin/paginas/

--- app/controllers/admin/paginas_controller.php
+++ app/controllers/admin/paginas_controller.php
@@ -4,9 +4,26 @@
  *
  * Controlador para crear, editar y borrar paginas
  */
+View::template('admin/admin');
+
 class PaginasController extends AppController 
 {
 	public function index()
+	{
+		
+	}
+        
+        public function ver()
+	{
+		
+	}
+        
+        public function editar()
+	{
+		
+	}
+        
+        public function borrar()
 	{
 		
 	}
