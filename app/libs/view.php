36e9fd9fa471c0de2fae09dddbb945239726361e
joanhey <joanhey@kumbiaphp.com>
2011-12-28 18:01:01 +0100

    app inicial de kumbiaphp

---
+++ app/libs/view.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Esta clase permite extender o modificar la clase ViewBase de Kumbiaphp.
+ *
+ * @category KumbiaPHP
+ * @package View
+ **/
+
+// @see KumbiaView
+require_once CORE_PATH . 'kumbia/kumbia_view.php';
+
+class View extends KumbiaView {
+
+}
