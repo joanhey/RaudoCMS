898f8bd332675ebb826e44994bd6e91a7a64201e
demonio <distrotuz@gmail.com>
2011-12-29 18:39:37 +0100

    creado los campos dir y pagina para darle magia al boton save

--- app/controllers/admin/pagina_controller.php
+++ app/controllers/admin/pagina_controller.php
@@ -10,19 +10,19 @@ class PaginaController extends AppController
 {
 	public function index()
 	{
-		if ( ! empty( $_POST['accion'] ) )
+		if ( ! empty( $_POST['dir'] ) and ! empty( $_POST['pagina'] ) )
 		{
-			$accion = ( $_POST['accion'] == 'borrar' ) ? 'borrar' : 'editar';
-			$this->$accion( $_POST );
-			return Router::redirect( '/admin/cms/index?dir=' . $_POST['pagina'] );
+			$this->editar( $_POST );
+			return Router::redirect( "/admin/cms/index?dir={$_POST['dir']}/{$_POST['pagina']}" );
 		}
+		return Router::redirect( '/admin/cms' );
 	}
         
     public function editar( $post )
 	{
-		if ( strstr( $post['pagina'], APP_PATH . 'views/pages' ) )
+		if ( strstr( $post['dir'], APP_PATH . 'views/pages' ) )
 		{
-			if ( _fs::updateFile( $post['pagina'], $post['codigo'] ) )
+			if ( _fs::updateFile( "{$post['dir']}/{$post['pagina']}", $post['codigo'] ) )
 			{
 				$_SESSION['flash'] = Flash::valid( 'Página actualizada' );
 			}
@@ -30,21 +30,21 @@ class PaginaController extends AppController
 			{
 				$_SESSION['flash'] = Flash::error( 'Error editando' );
 			}
-			return Router::redirect( '/admin/cms/index?dir=' . $post['pagina'] );
+			return Router::redirect( "/admin/cms/index?dir={$post['dir']}/{$post['pagina']}" );
 		}
 		return Router::redirect( '/admin/cms' );
 	}
         
     public function borrar( $post )
 	{
-			if ( _fs::deleteFile( $post['pagina'] ) )
-			{
-				$_SESSION['flash'] = Flash::valid( 'Página borrada' );
-			}
-			else
-			{
-				$_SESSION['flash'] = Flash::error( 'Error borrando' );
-			}
-			return Router::redirect( '/admin/cms/index?dir=' . basename( $post['pagina'] ) );
+		/*if ( _fs::deleteFile( $post['pagina'] ) )
+		{
+			$_SESSION['flash'] = Flash::valid( 'Página borrada' );
+		}
+		else
+		{
+			$_SESSION['flash'] = Flash::error( 'Error borrando' );
+		}
+		return Router::redirect( '/admin/cms/index?dir=' . basename( $post['pagina'] ) );*/
 	}
 }
